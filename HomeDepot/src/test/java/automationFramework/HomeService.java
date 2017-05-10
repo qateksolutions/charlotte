8f7446948d488aba9b9eb2f1beccd159bc9cb98c
Unknown <alam08215@hotmail.com>
2017-05-09 20:29:32 -0400

    Created new TestNG class in Automation folder

--- HomeDepot/src/test/java/automationFramework/HomeService.java
+++ HomeDepot/src/test/java/automationFramework/HomeService.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
-public class DYIProject {
+public class HomeService {
 
 	@BeforeTest
 	public void beforeTest() {
