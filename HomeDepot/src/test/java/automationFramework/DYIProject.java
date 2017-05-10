93fa81cdc625e2723d3d066c98bbb55618030767
Unknown <alam08215@hotmail.com>
2017-05-09 20:20:56 -0400

    Created a TestNG Class

---
+++ HomeDepot/src/test/java/automationFramework/DYIProject.java
@@ -0,0 +1,22 @@
+package automationFramework;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class DYIProject {
+
+	@BeforeTest
+	public void beforeTest() {
+	}
+	
+	@Test
+	public void f() {
+	}
+
+
+	@AfterTest
+	public void afterTest() {
+	}
+
+}
